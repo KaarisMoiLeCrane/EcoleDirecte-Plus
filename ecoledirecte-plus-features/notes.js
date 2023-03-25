b64790f1f040768156c4a83fd2bed30e72cdf259
KaarisMoiLeCrane <77919608+KaarisMoiLeCrane@users.noreply.github.com>
2023-03-25 23:26:21 +0100

    Update notes.js
    Ajout de la simulation de moyennes (ajouter des notes par sois même et calculer la moyenne simulée).
    Adding the simulation of averages (adding grades by yourself and calculating the simulated average).

--- ecoledirecte-plus-features/notes.js
+++ ecoledirecte-plus-features/notes.js
@@ -55,6 +55,7 @@ globalThis.notes = function (id) {
 
 function mainNotes(note) {
     try {
+		mainAjouterNote()
         console.log(456)
         // If there is already two things with the class "coef" etc, it means that our new category "Rang. If not, we apply our changes
         if (!document.querySelectorAll("th[class *= 'coef ng-star-inserted']")[1]) {
@@ -176,7 +177,12 @@ function mainNotes(note) {
                     // Duplicate the average element of the subject to add in a the second line the average calculated by EcoleDirecte Plus
                     let averageElement = lignes[i].querySelector("td.relevemoyenne").cloneNode(true);
                     averageElement.textContent = moyenne.toFixed(5)
-                    lignes[i].querySelector("td.relevemoyenne").innerHTML = lignes[i].querySelector("td.relevemoyenne").innerHTML + "<br>" + averageElement.innerHTML;
+					
+					if (!lignes[i].querySelector("[class *= 'kmlc-moyenne']")) {
+						lignes[i].querySelector("td.relevemoyenne").innerHTML = lignes[i].querySelector("td.relevemoyenne").innerHTML + '<br><span class="kmlc-moyenne">' + averageElement.innerHTML + '</span>';
+					} else {
+						lignes[i].querySelector("[class *= 'kmlc-moyenne']").textContent = moyenne.toFixed(5)
+					}
                     
                     // We multiply the average of the subject with his coefficient and we add it to the overall average
                     matieresMoyenne += moyenne * coeffMat;
@@ -192,16 +198,233 @@ function mainNotes(note) {
                 overallAverageElement.textContent = moyenneG.toFixed(5);
                 console.log(9, document.querySelector("tr > td.moyennegenerale-valeur"))
 
-                document.querySelector("tr > td.moyennegenerale-valeur").innerHTML = document.querySelector("tr > td.moyennegenerale-valeur").innerHTML + "<br>" + overallAverageElement.innerHTML;
-            } else {
+				if (!document.querySelector("[class *= 'kmlc-moyenne-g']")) {
+					document.querySelector("tr > td.moyennegenerale-valeur").innerHTML = document.querySelector("tr > td.moyennegenerale-valeur").innerHTML + '<br><span class="kmlc-moyenne-g">' + overallAverageElement.innerHTML + '</span>';
+				} else {
+					document.querySelector("[class *= 'kmlc-moyenne-g']").textContent = moyenneG.toFixed(5)
+				}
+			} else {
                 let overallAverageElement = document.createElement("tr")
-                overallAverageElement.innerHTML = '<tr class="ng-star-inserted"><td colspan="2" class="text-right moyennegeneralelibelle">Moyenne générale</td><td colspan="2" class="moyennegenerale-valeur">' + moyenneG.toFixed(5); + '</td></tr>'
+                overallAverageElement.innerHTML = '<tr class="ng-star-inserted"><td colspan="2" class="text-right moyennegeneralelibelle">Moyenne générale</td><td colspan="2" class="moyennegenerale-valeur"><span class="kmlc-moyenne-g">' + moyenneG.toFixed(5); + '</span></td></tr>'
                 console.log(10, overallAverageElement, document.querySelector("table.ed-table tbody"))
                 
-                document.querySelector("table.ed-table tbody").appendChild(overallAverageElement)
+				if (!document.querySelector("[class *= 'kmlc-moyenne-g']")) {
+					document.querySelector("table.ed-table tbody").appendChild(overallAverageElement)
+				} else {
+					document.querySelector("[class *= 'kmlc-moyenne-g']").textContent = moyenneG.toFixed(5)
+				}
             }
 
             console.log(moyenneG, matieresMoyenne, coeffMatTot)
         }
     } catch(e){}
+}
+
+function mainAjouterNote() {
+	if (!document.querySelector("[class *= 'kmlc-text-note']")) {
+		let textSimu = document.querySelector("table caption").parentElement.getElementsByContentText("(note)").startsWith[0].cloneNode(true)
+		textSimu.className = "kmlc-text-note"
+		
+		textSimu.children[0].textContent = "note"
+		textSimu.children[0].setAttribute("style", "color: green;")
+		textSimu.children[1].textContent = "Note ajoutée pour simulation"
+		textSimu.className = "kmlc-text-note"
+		
+		document.querySelector("table caption").parentElement.getElementsByContentText("(note)").startsWith[0].insertAfter(textSimu)
+	}
+	
+	if (!document.querySelector("[class *= 'kmlc-bouton-note']")) {
+		let ajoutNote = document.querySelector("ul > li.active.nav-item.ng-star-inserted").cloneNode(true)
+		ajoutNote.className = ajoutNote.className.replace("active", " kmlc-bouton-note ")
+		
+		ajoutNote.children[0].removeAttribute("href")
+		ajoutNote.children[0].children[0].textContent = "Ajouter une note"
+		ajoutNote.addEventListener('click', function(e) {
+			e.stopPropagation()
+			e.preventDefault()
+			
+			/*let backdrop = document.createElement("BS-MODAL-BACKDROP")
+			backdrop.className = "modal-backdrop fade in"
+			backdrop.addEventListener('click', function(e) {
+				e.stopPropagation()
+				e.preventDefault()
+				
+				this.className = this.className.replace("in", "out")
+				
+				setTimeout(() => {this.remove()}, 500);
+			})
+			document.body.appendChild(backdrop)*/
+			
+			let promptMat = prompt("Matière (La même que affiché dans le tableau)");
+			if (promptMat != "") {
+				promptTitre = prompt("Titre (Pour contextualiser la note)")
+				if (promptTitre == "") promptTitre = "Évaluation"
+				
+				promptNote = prompt("Note (Un nombre seulement)")
+				if (promptNote != "") {
+					promptNote = promptNote.replace(/[()\/\s]/g, "").replace(",", ".").replace(/[^\d+\-*/.\s]/g, "")
+					
+					promptCoeff = prompt("Coefficient (Un nombre seulement)")
+					promptCoeff = promptCoeff.replace(/[()\/\s]/g, "").replace(",", ".").replace(/[^\d+\-*/.\s]/g, "")
+					if (promptCoeff == "") promptCoeff = 1
+					
+					promptQuotient = prompt("Quotient (Un nombre seulement)")
+					promptQuotient = promptQuotient.replace(/[()\/\s]/g, "").replace(",", ".").replace(/[^\d+\-*/.\s]/g, "")
+					if (promptQuotient == "") promptQuotient = 20
+					
+					ajouterNote(promptMat, promptTitre, promptNote, promptCoeff, promptQuotient)
+				}
+			}
+			// ajouterNote(prompt("Matière (La même que affiché dans le tableau)"), prompt("Titre (Pour contextualiser la note)"), prompt("Note (Un nombre seulement)"), prompt("Coefficient (Un nombre seulement)"), prompt("Quotient (Un nombre seulement)"))
+		})
+		
+		document.querySelector("ul > li.active.nav-item.ng-star-inserted").parentElement.insertBefore(ajoutNote, document.querySelector("ul > li.active.nav-item.ng-star-inserted"))
+	}
+}
+
+function ajouterNote(matiere, titre, note, coeff, quotient) {
+	let noteElement = document.createElement("BUTTON")
+	noteElement.className = "kmlc-note-simu"
+	
+	let listMatiere = document.querySelectorAll("[class *= nommatiere]")
+	
+	for (let i = 0; i < listMatiere.length; i++) {
+		if (listMatiere[i].textContent == matiere) {
+			listMatiere[i].parentElement.parentElement.querySelector("[class *= 'notes']").appendChild(noteElement)
+			listMatiere[i].parentElement.parentElement.querySelector("[class = 'kmlc-note-simu']").outerHTML = '<button type="button" class="note cliquable margin-whitespace btn-simple no-background no-padding ng-star-inserted" title=" '+ titre + '"><!----><span class="valeur ng-star-inserted" style="color: green;"> ' + note + ' <!----><sup class="coef ng-star-inserted"> ' + coeff + ' <span class="margin-whitespace"></span></sup><!----><sub class="coef ng-star-inserted"> /' + quotient + ' <span class="margin-whitespace"></span></sub></span><!----><!----><!----></button>'
+		}
+	}
+	
+	// We get all the displayed grades
+	let matNotes = document.querySelectorAll("span.valeur");
+	console.log(3, matNotes)
+	let lignes = [];
+	
+	// We init the overall average, the value of all the subjects average, the excessed coefficients (coefficients counted when there are no grades) and the total coefficients (that he will divide all the grades to get the overall average)
+	let moyenneG = 0.0
+	let matieresMoyenne = 0.0;
+	let coeffEnTrop = 0.0;
+	let coeffMatTot = 0.0;
+	
+	// For each grades, get the row containing all the datas about the subject and then push each row element, if they are not in the list, to our lignes list
+	for (let i = 0; i < matNotes.length; i++) {
+		let parent = matNotes[i].parentElement.parentElement.parentElement;
+		console.log(4, parent, matNotes[i])
+		if (!lignes.includes(parent)) {
+			lignes.push(parent);
+		}
+	}
+
+	// For each rows, get all the grades in the row (so all the grades of a specific subject), and calculate the average of the subject
+	for (let i = 0; i < lignes.length; i++) {
+		
+		// Get all the grades in the row, init the overall average of the subject and init the vraiable to set the total coefficients of all the grades in the subject
+		let matiereNotes = lignes[i].querySelectorAll("span.valeur")
+		console.log(5, matiereNotes)
+		let moyenne = 0.0;
+		let addNotes = 0.0;
+		let coeffNoteTot = 0.0;
+		
+		// For each grades of the suject, get his coefficient and his quotient and we calculate everything. We convert the grade to be /20 and we multiply it with his coefficient. We add the coefficient to the coeffNoteTot
+		for (let j = 0; j < matiereNotes.length; j++) {
+			let coeff = 1.0
+			let quotient = 20.0
+			
+			// Get the coefficient of the grade
+			if (matiereNotes[j].querySelector("sup")) {
+				// Regex to replace "," with "." and the spaces with nothing
+				coeff = parseFloat(matiereNotes[j].querySelector("sup").textContent.replace(/[()\/\s]/g, "").replace(",", "."));
+			}
+			
+			// Get the quotient of the grade
+			if (matiereNotes[j].querySelector("sub")) {
+				// Regex to replace "," with "." and the spaces with nothing
+				quotient = parseFloat(matiereNotes[j].querySelector("sub").textContent.replace(/[()\/\s]/g, "").replace(",", "."));
+			}
+
+			// Get the grade and replace all the spaces and letters with nothing and the "," with "."
+			let matNote = matiereNotes[j].childNodes[0].nodeValue
+			
+			// A grade between two parentheses is not a grade
+			if (!matNote.includes("(") && !matNote.includes(")")) {
+				matNote = matNote.replace(/[()\/\s]/g, "").replace(",", ".").replace(/[^\d+\-*/.\s]/g, "")
+			} else {
+				matNote = NaN
+			}
+			
+			console.log(6, matNote, matiereNotes[j].childNodes[0].nodeValue)
+
+			// If there is a grade (0 is a grade but nothing and a grade between two parentheses is not a grade). matNote is a string so ``if ("0" || "0.0") console.log(1)`` will log 1
+			if (matNote) {
+				console.log(6.1, matNote)
+				
+				// Convert the grade to /20
+				matNote = parseFloat(matNote)*20.0/quotient
+				console.log(6.2, matNote, coeff)
+				
+				// Multiply it with the coefficient of the grade, add it to the average value of the subject and add the coefficient with the total of coefficients of the subject
+				addNotes += parseFloat(matNote*coeff);
+				coeffNoteTot += coeff;
+				console.log(matiereNotes[j], addNotes, coeffNoteTot, coeff)
+			}
+		}
+		
+		// Get the coefficient of the subject
+		let coeffMat = parseFloat(lignes[i].querySelector("td.coef:not([class *= 'text-center'])").innerText.replace(/[()\/\s]/g, "").replace(",", ".").replace(/[^\d+\-*/.\s]/g, ""));
+		console.log(7, coeffMat)
+		
+		// If the total of coefficients of the subject is 0 (so no grades to count)
+		if (coeffNoteTot ==  0.0) {
+			coeffEnTrop += coeffMat;
+			console.log(8, coeffMat, coeffNoteTot, addNotes, parseFloat(lignes[i].querySelector("td.coef:not([class *= 'text-center'])").innerText))
+		} else {
+			
+			// We add the coefficient of the subject to the total of coefficient of the subjects
+			coeffMatTot += coeffMat;
+			
+			// We calculate the average of the subject
+			moyenne = (addNotes/coeffNoteTot);
+			console.log(moyenne);
+
+			// Duplicate the average element of the subject to add in a the second line the average calculated by EcoleDirecte Plus
+			let averageElement = lignes[i].querySelector("td.relevemoyenne").cloneNode(true);
+			averageElement.textContent = moyenne.toFixed(5)
+			if (!lignes[i].querySelector("[class *= 'kmlc-simu-moyenne']")) {
+				lignes[i].querySelector("td.relevemoyenne").innerHTML = lignes[i].querySelector("td.relevemoyenne").innerHTML + '<br><span class="kmlc-simu-moyenne" style="color: green;">' + averageElement.innerHTML + '</span>';
+			} else {
+				lignes[i].querySelector("[class *= 'kmlc-simu-moyenne']").textContent = moyenne.toFixed(5)
+			}
+			
+			// We multiply the average of the subject with his coefficient and we add it to the overall average
+			matieresMoyenne += moyenne * coeffMat;
+		}
+	}
+	
+	// We calculate the overall average
+	moyenneG = matieresMoyenne/coeffMatTot
+	
+	// If there is the overall average row we add our overall average in the second. If not, we create it and put it in the second line as well (the first line is blank)
+	if (document.querySelector("tr > td.moyennegenerale-valeur")) {
+		let overallAverageElement = document.querySelector("tr > td.moyennegenerale-valeur").cloneNode(true)
+		overallAverageElement.textContent = moyenneG.toFixed(5);
+		console.log(9, document.querySelector("tr > td.moyennegenerale-valeur"))
+
+		if (!document.querySelector("[class *= 'kmlc-simu-moyenne-g']")) {
+			document.querySelector("tr > td.moyennegenerale-valeur").innerHTML = document.querySelector("tr > td.moyennegenerale-valeur").innerHTML + '<br><span class="kmlc-simu-moyenne-g" style="color: green;">' + overallAverageElement.innerHTML + '</span>'
+		} else {
+			document.querySelector("[class *= 'kmlc-simu-moyenne-g']").textContent = moyenneG.toFixed(5)
+		}
+	} else {
+		let overallAverageElement = document.createElement("tr")
+		overallAverageElement.innerHTML = '<tr class="ng-star-inserted"><td colspan="2" class="text-right moyennegeneralelibelle">Moyenne générale</td><td colspan="2" class="moyennegenerale-valeur"><span class="kmlc-simu-moyenne-g" style="color: green;">' + moyenneG.toFixed(5); + '</span></td></tr>'
+		console.log(10, overallAverageElement, document.querySelector("table.ed-table tbody"))
+		
+		if (!document.querySelector("[class *= 'kmlc-simu-moyenne-g']")) {
+			document.querySelector("table.ed-table tbody").appendChild(overallAverageElement)
+		} else {
+			document.querySelector("[class *= 'kmlc-simu-moyenne-g']").textContent = moyenneG.toFixed(5)
+		}
+	}
+
+	console.log(moyenneG, matieresMoyenne, coeffMatTot)
 }
