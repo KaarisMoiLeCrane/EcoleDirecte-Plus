7e24cc2aee1b1f5b715c642da853a60f68c7c937
KaarisMoiLeCrane <77919608+KaarisMoiLeCrane@users.noreply.github.com>
2023-04-25 18:57:31 +0200

    Update design.js

--- ecoledirecte-plus-features/design.js
+++ ecoledirecte-plus-features/design.js
@@ -1,4 +1,4 @@
-// https://github.com/Bastian-Noel/CustomDirecte
+// https://github.com/Bastian-Noel/CustomDirecte (CSS volé à cette personne avec des changements mais JS à 100% par moi)
 
 globalThis.design = function () {
 	globalThis.Design.sidebar()
