535d9bc7bc9bad9a40c628874428d422d71acd5d
KaarisMoiLeCrane <77919608+KaarisMoiLeCrane@users.noreply.github.com>
2023-12-31 11:28:20 +0100

    git Fix Test

--- features/Notes/functions/calculer-objectif-moyenne.js
+++ features/Notes/functions/calculer-objectif-moyenne.js
@@ -82,6 +82,65 @@
     const globalMeanElement = document.querySelector('[kmlc-moyenne-g]')
 
     ajouterObjectifNote(globalMeanElement, calculatedSubjectGoalGlobalMean, globalMeanGoalId, 'kmlc-global-mean-objectif-set', 'kmlc-global-mean-parent', 'kmlc-global-mean-objectif')
+    
+    
+    // const globalMean = Number(
+    //   globalMeanElement.textContent.split(' ')[0]
+    // );
+
+    // let tooltipClass = ' kmlc-tooltip-red';
+
+    // // console.log(matiereNote)
+
+    // if (globalMean > calculatedSubjectGoalGlobalMean) {
+    //   backgroundColor = ' background-color: rgb(0, 255, 0, 0.5);';
+    //   tooltipClass = ' kmlc-tooltip-green';
+    // } else if (globalMean < calculatedSubjectGoalGlobalMean) {
+    //   backgroundColor = ' background-color: rgb(255, 0, 0, 0.5);';
+    //   tooltipClass = ' kmlc-tooltip-red';
+    // } else {
+    //   backgroundColor = ' background-color: rgb(255, 255, 255);';
+    //   tooltipClass = '';
+    // }
+
+    // if (
+    //   globalMean.toString().split('.')[0] ==
+    //   calculatedSubjectGoalGlobalMean.toString().split('.')[0]
+    // ) {
+    //   backgroundColor = ' background-color: rgb(255, 127.5, 0, 0.5);';
+    //   tooltipClass = ' kmlc-tooltip-orange';
+    // }
+
+    // if (!globalMeanElement.parentElement.getAttribute('kmlc-global-mean-objectif-set')) {
+    //   // console.log("objectif 2", backgroundColor, gradeValue, matiereNote)
+
+    //   globalMeanElement.parentElement.className += ' kmlc-global-mean-parent';
+
+    //   const tooltipElement = globalMeanElement.cloneNode(true);
+    //   tooltipElement.className += tooltipClass;
+    //   tooltipElement.textContent = 'Objectif de ' + calculatedSubjectGoalGlobalMean;
+    //   tooltipElement.setAttribute("style", "font-weight: 400;")
+    //   tooltipElement.setAttribute('kmlc-global-mean-objectif', 'true');
+    //   tooltipElement.removeAttribute('kmlc-moyenne-g');
+
+    //   globalMeanElement.parentElement.appendChild(tooltipElement);
+
+    //   globalMeanElement.parentElement.setAttribute('style', backgroundColor);
+
+    //   globalMeanElement.parentElement.setAttribute('kmlc-global-mean-objectif-set', 'true');
+    // } else {
+    //   // console.log("objectif 2", backgroundColor, gradeValue, matiereNote)
+
+    //   const tooltipElement = globalMeanElement.parentElement.querySelector(
+    //     "span[class *= 'kmlc-tooltip']"
+    //   );
+    //   tooltipElement.className = tooltipClass;
+    //   tooltipElement.textContent = 'Objectif de ' + calculatedSubjectGoalGlobalMean;
+    //   tooltipElement.setAttribute("style", "font-weight: 400;")
+    //   tooltipElement.setAttribute('kmlc-global-mean-objectif', 'true');
+
+    //   globalMeanElement.parentElement.setAttribute('style', backgroundColor);
+    // }
   }
 
   exports({calculerObjectifNote}).to('./features/Notes/functions/calculer-objectif-moyenne.js')
